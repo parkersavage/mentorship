ebe1d009e6c3004e091e8ca3ffe2d5955c0b6cf3
Parker Savage <68204938+parkersavage@users.noreply.github.com>
2022-02-10 11:03:09 -0600

    Update Voting.sol

--- Voting.sol
+++ Voting.sol
@@ -2,25 +2,32 @@ pragma solidity ^0.7.0;
 pragma abicoder v2;
 
 contract Voting{
-    string[3][] listElections;
+   address admin;
+   string title, description;
+   string[] options;
+   uint256[] tally;
+
+   constructor(){
+        admin = msg.sender
+    }
 
     struct Election{
         string title;
+        string description;
         string[] options;
-        uint256 numOfVotes;
+        uint256[] tally;
     }
 
     struct Voter{
         bool voted;
-        uint vote; /// index of the voted election
+        uint vote;
     }
 
-    function createNewElection(string memory name, string memory inte, string memory stri) public{
-        /// string[] memory candidates
-        listElections.push([name, inte, stri]);
-    }
-
-    function getListElections() public view returns (string [] memory){
-        return listElections;
+    function createNewElection(string memory _title, string memory _description, string[] memory _options, uint256[] memory _tally) public{
+        require(msg.sender==admin);
+        title = _title;
+        description = _description;
+        options = _options;
+        tally = _tally;
     }
 }
