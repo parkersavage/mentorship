49f657d53bc8ab72539c811546207cee2a02b11a
Parker Savage <68204938+parkersavage@users.noreply.github.com>
2022-02-09 20:16:48 -0600

    upload voting.sol
    Rough rough roughhhhh draft of voting smart contract

--- Voting.sol
+++ Voting.sol
@@ -1,1 +1,26 @@
+pragma solidity ^0.7.0;
+pragma abicoder v2;
 
+contract Voting{
+    string[3][] listElections;
+
+    struct Election{
+        string title;
+        string[] options;
+        uint256 numOfVotes;
+    }
+
+    struct Voter{
+        bool voted;
+        uint vote; /// index of the voted election
+    }
+
+    function createNewElection(string memory name, string memory inte, string memory stri) public{
+        /// string[] memory candidates
+        listElections.push([name, inte, stri]);
+    }
+
+    function getListElections() public view returns (string [] memory){
+        return listElections;
+    }
+}
